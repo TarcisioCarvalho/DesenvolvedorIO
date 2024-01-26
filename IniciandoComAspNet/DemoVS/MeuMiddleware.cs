d2760cddc51a931882eb4bff56901408ddf31933
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2024-01-25 22:48:49 -0300

    TestAppGet

---
+++ IniciandoComAspNet/DemoVS/MeuMiddleware.cs
@@ -0,0 +1,16 @@
+﻿namespace PorBaixoDosPanos
+{
+    public class TemplateMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public TemplateMiddleware(RequestDelegate next) 
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            await _next(httpContext);
+        }
+    }
+}
